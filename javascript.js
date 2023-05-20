969083fa9265869539c52c618c6a5aada06dc28a
Rosario <118317818+Melly2929@users.noreply.github.com>
2023-05-20 16:51:19 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let nombre ="Rosario"
+let apellido = "Lassalle"
+
+console.log("Nombre y apellido", nombre +" "+ apellido);
+
+let num1 = 15
+let num2 = 20
+
+let suma = num1 + num2
+
+console.log( "Suma", suma) 
+
+let suma2 = 7 + suma
+
+console.log ("Segunda suma", suma2)
